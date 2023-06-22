9311114e26d1733aa798a9376a0b5a63267050c7
tests2 <emireseno94@gmail.com>
2023-06-22 06:15:58 +0000

    first commit

--- awsdocker.sh
+++ awsdocker.sh
@@ -2,8 +2,10 @@
 
 git init
 git config --global --add safe.directory /home/test/my-project
+git config --global user.email emireseno94@gmail.com
 echo "# AwsDocker" >> README.md
 git add README.md
+git add awsdocker.sh
 git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:Emir3339/AwsDocker.git
